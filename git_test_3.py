3b8ce416f527b647ebb9066dc99cdd44e4b2cc6e
JonErvin <58059331+JonErvin@users.noreply.github.com>
2022-01-29 12:11:56 -0600

    Add files via upload
    Testing pull

---
+++ git_test_3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 29 12:04:24 2022
+
+@author: Jon Ervin
+"""
+
+# Git test 3
+# Goal: Add manually and then sync to local report with git pull
+
+print('Created in different folder')
